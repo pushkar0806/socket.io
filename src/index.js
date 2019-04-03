5e1ba64e2e9f8a749b5d96827ee14388eafc2791
pushkar <pushkar.abhishek@neosofttech.com>
2019-04-03 13:53:03 +0530

    Modified the environment variable

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const { addUser, removeUser, getUser, getUserInRoom } = require('./utils/users')
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
